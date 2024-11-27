93092f809ea072c87858bcbd2973c222a0f84083
egetelli <egetelli@hotmail.com>
2024-11-27 13:47:36 +0300

    Adding product controller and views pt2

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -13,8 +13,30 @@ namespace BulkyBook.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        [MaxLength(30)]
-        [DisplayName("Category Name")]
-        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1,1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
     }
 }
